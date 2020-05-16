90f259d77b24c9f8e8e484676e9287e4d1930a7f
DaveyCHaysIII <63436710+DaveyCHaysIII@users.noreply.github.com>
2020-05-16 11:32:40 -0500

    switched to branch structure
    I messed up and kept putting everything to master, which overwrites and ruins the stuff my collaborator was working on. Now stuff goes here.

---
+++ testing.js
@@ -0,0 +1,36 @@
+class Super{
+    constructor(){
+        this.mod = this.getMod();
+        this.thing = this.getThing();
+        
+    }
+    getThing() {
+        return new Sub();
+    }
+    getMod() {
+        return Math.floor(Math.random()*3)+1;
+    }
+}
+
+class Sub{
+    constructor(){
+        this.name = this.getName();
+    }
+    getName() {
+        // if(whatever.mod ==1){
+        //     return 'thing1';
+        // } else if(whatever.mod == 2){
+        //     return 'thing2';
+        // } else if(whatever.mod == 3){
+        //     return 'thing3';
+        // }
+        return 'thing1'
+    }
+}
+
+const whatever = new Super();
+console.log(whatever.thing.name);
+
+//testing result:
+//cannot access "whatever" before initialization
+//we can't access the properties of the parent object from the child object
